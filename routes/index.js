e60c45cbe70df00ca0824a904e241d6ec2b0ac5e
Noroff BED <124564549+noroff-bed1@users.noreply.github.com>
2024-02-28 16:58:08 +0200

    added /status endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var os = require('os');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  let student = {
-    name: "myname",
-    surname: "surname"
+router.get('/', (req, res, next) => {
+	let student = {
+		name: 'myname',
+		surname: 'surname',
+	};
+	res.render('index', { title: 'CDS Resit 1', user: student });
+});
 
-  }
-	res.render('index', { title: 'CDS Resit 1', user:student });
+router.get('/status', (req, res, next) => {
+	res.status(200).json({ hostname: os.hostname(), homedir: os.homedir(), network:os.networkInterfaces() });
 });
 
 module.exports = router;
